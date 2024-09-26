01a3de609d20f5003e8783b51c8352e0f08d8e88
Daniel Friyia <dfriyia@gmail.com>
2024-09-26 18:54:24 -0400

    module complete

---
+++ modules/coodinate-tracker/android/src/main/java/expo/modules/coodinatetracker/CoodinateTrackerView.kt
@@ -0,0 +1,7 @@
+package expo.modules.coodinatetracker
+
+import android.content.Context
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+
+class CoodinateTrackerView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
---
+++ modules/coodinate-tracker/ios/CoodinateTrackerModule.swift
@@ -0,0 +1,11 @@
+import ExpoModulesCore
+
+public class CoodinateTrackerModule: Module {
+  public func definition() -> ModuleDefinition {
+    Name("CoodinateTracker")
+
+    AsyncFunction("initializeAI") {
+      
+    }
+  }
+}
