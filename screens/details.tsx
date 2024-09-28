31b678ad068ecd7d699152354d86d190f65d1947
Daniel Friyia <dfriyia@gmail.com>
2024-09-28 11:02:22 -0400

    Finishing touch

--- screens/details.tsx
+++ screens/details.tsx
@@ -3,7 +3,6 @@ import { useXContext } from './context';
 
 import {
   Canvas,
-  Circle,
   fitbox,
   Image,
   LinearGradient,
@@ -13,53 +12,94 @@ import {
   useVideo,
   vec,
 } from '@shopify/react-native-skia';
-import { useSharedValue } from 'react-native-reanimated';
+import { useDerivedValue, useSharedValue } from 'react-native-reanimated';
 import { useEffect } from 'react';
 
 const { width, height } = Dimensions.get('screen');
 
+const ADJUST_Y = -5;
+const ADJUST_X = 15;
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+export const distance = (p1: Point, p2: Point) => {
+  'worklet';
+  return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+};
+
 export default function Details() {
   const { videoFile, observations } = useXContext();
 
-  // const paused = useSharedValue(true);
-  // const seek = useSharedValue(1);
-  // const looping = useSharedValue(false);
-  // const volume = useSharedValue(0);
+  const paused = useSharedValue(true);
+  const seek = useSharedValue(1);
+  const looping = useSharedValue(false);
+  const volume = useSharedValue(0);
 
-  // const { currentFrame, rotation, size, currentTime } = useVideo(videoFile, {
-  //   paused,
-  //   seek,
-  //   looping,
-  //   volume,
-  // });
+  const { currentFrame, rotation, size, currentTime } = useVideo(videoFile, {
+    paused,
+    seek,
+    looping,
+    volume,
+  });
 
-  // useEffect(() => {
-  //   paused.value = false;
-  //   setTimeout(() => {
-  //     paused.value = true;
-  //   }, 250);
-  // }, []);
+  useEffect(() => {
+    paused.value = false;
+    setTimeout(() => {
+      paused.value = true;
+    }, 250);
+  }, []);
 
-  // const onRestartPressed = () => {
-  //   paused.value = true;
-  //   seek.value = 1;
+  const onRestartPressed = () => {
+    paused.value = true;
+    seek.value = 1;
 
-  //   setTimeout(() => {
-  //     paused.value = false;
-  //   }, 300);
-  // };
+    setTimeout(() => {
+      paused.value = false;
+    }, 300);
+  };
 
-  // const src = rect(0, 0, size.width, size.height);
-  // const dst = rect(0, 0, width, height);
-  // const transform = fitbox('fill', src, dst, rotation);
+  const path = useDerivedValue(() => {
+    const path = Skia.Path.Make();
+
+    const firstPoint = {
+      x: observations[0].x - ADJUST_X,
+      y: observations[0].y - ADJUST_Y,
+    };
+    path.moveTo(firstPoint.x, firstPoint.y);
+
+    for (const obs of observations) {
+      const frameTime = Number(obs.frameTime) + 0.1;
+
+      const d = distance(
+        {
+          x: obs.x - ADJUST_X,
+          y: obs.y - ADJUST_Y,
+        },
+        firstPoint
+      );
+
+      if (Number(frameTime) * 1000 <= currentTime.value) {
+        path.lineTo(obs.x - ADJUST_X, obs.y - ADJUST_Y);
+        if (d < 50 && Number(frameTime) * 1000 > 1000) {
+          return path;
+        }
+      }
+    }
+
+    return path;
+  }, [currentTime]);
+
+  const src = rect(0, 0, size.width, size.height);
+  const dst = rect(0, 0, width, height);
+  const transform = fitbox('fill', src, dst, rotation);
 
   return (
     <>
       <Canvas style={StyleSheet.absoluteFill}>
-        {observations.map((obs) => {
-          return <Circle r={8} cx={obs.x} cy={obs.y} color={'red'} />;
-        })}
-        {/* <Image
+        <Image
           image={currentFrame}
           x={0}
           y={0}
@@ -67,9 +107,12 @@ export default function Details() {
           height={width * 3}
           fit="cover"
           transform={transform}
-        /> */}
+        />
+        <Path strokeCap="round" path={path} color="red" style="stroke" strokeWidth={8}>
+          <LinearGradient start={vec(0, 100)} end={vec(600, 500)} colors={['#D60000', '#FFFDD0']} />
+        </Path>
       </Canvas>
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
+      <TouchableOpacity style={styles.button} onPress={onRestartPressed}>
         <Text style={styles.buttonText}>Start Video</Text>
       </TouchableOpacity>
     </>
